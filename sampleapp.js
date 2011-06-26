12a2256309d0aa22fb57cc07e3a71cb9fe2d6a42
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-06-26 14:09:28 +0200

    storyline...

---
+++ sampleapp.js
@@ -0,0 +1,14 @@
+storyLine= { 
+    start: {
+        text: "You are the thief Jarek, after a successfull burglery in upper Everran you are seated in a tavern waiting for your good friend Thomas who has asked you to meet him, his letter stated that it was urgent. The banter in the bar is boring and you find yourself having waited for an hour without him showing. What will you do. Wait or go look for him",
+        regions: [
+            {x:.5, y:0, w:.5, h:1, screen: "waitForThomas"},
+            {x:0, y:0, w:.5, h:1, screen: "lookForThomas"}]
+    },
+    waitForThomas: {
+        text: "You decide to wait for Thomas, you order another beer and prepare to wait a little longer for your friend. A cloacked figutre steps into the room you can tell it is a woman, and your trained eyes tell you that she is armed. probably handcrossbow and a rapier you are not sure, but here cape is unneceasarry wide at the shoulders. As she turns towards the bar a lock of red hair spills out from under the cloak, there sound of the other patrons conversations and laughter makes her words inaudible but you are able to read her lips as she asks for Thomas... As you are about to decide what to do, Thomas stumbles through the door, blood spilling from a wound in his gut. What do you do?"
+    },
+    lookForThomas: {
+        text: "Looking for Thomas: You step out into the foggy clouded night, the moon only partially reveals itself through the thick mist and there is a chill in the air. The streets are all but empty, you make your way towards his flat two blocks away. Outside his apartment you hear noices of struggle coming from his apartment. Do you kick in the door, or climb the building to look through the window?&apos;"
+    }
+}
